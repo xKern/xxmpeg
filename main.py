b30f553426f56eab0f8a3a948fbf894f1093b3c1
Haider <me@haiderali.dev>
2022-02-04 18:22:28 +0530

    upload example

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from xxmpeg import XXMPEG
 
-try:
-    a = XXMPEG(input_path='/home/neo/work/xkern/xmptest/input/aqua.wmv',
-               output_directory='/home/neo/work/xkern/xmptest/output/',
-               log_directory='/home/neo/work/xkern/xmptest/logsz/')
-    variants = a.output()
-    print(len(variants))
-except Exception as e:
-    print(str(e))
+a = XXMPEG(input_path='/home/neo/work/xkern/xmptest/input/testy.mp4',
+           output_directory='/home/neo/work/xkern/xmptest/output/',
+           log_directory='/home/neo/work/xkern/xmptest/logs/')
+video_object = a.output()
+print(video_object)
